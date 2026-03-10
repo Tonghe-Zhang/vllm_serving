ab020616d39ae269fc8684d7dfd36b913ddb19f6
jia-xie <103150184+jia-xie@users.noreply.github.com>
2026-03-10 13:38:13 -0400

    try box image.

--- client/run_qwen3_vl_client_sys_prompt.sh
+++ client/run_qwen3_vl_client_sys_prompt.sh
@@ -1,4 +1,4 @@
 uv run python ./vllm_serving/client/client_qwen3_vl.py  \
     --model Qwen3-VL-8B-Instruct \
-    --image ./vllm_serving/media/image.png \
+    --image ./vllm_serving/media/image-box.png \
     --system-prompt ./client/system_prompt/robot_teacher_career_guide.md
