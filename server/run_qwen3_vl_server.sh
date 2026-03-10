003a17fcdb1a40a45a2b0282beb57ae69f3d7700
Tonghe-Zhang <tonghezhang01@gmail.com>
2026-03-10 15:55:26 +0000

    feat(server): use relative model paths

--- server/run_qwen3_vl_server.sh
+++ server/run_qwen3_vl_server.sh
@@ -4,4 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$SCRIPT_DIR/../vllm_serving"
 
-uv run python server/serve_qwen3_vl.py --model-type instruct   --model-base-dir /usr0/tonghez/PretrainedModels   --tensor-parallel-size 4   --gpu-memory-utilization 0.65
+uv run python server/serve_qwen3_vl.py \
+--model-type instruct \
+--model-base-dir ../PretrainedModels \
+--tensor-parallel-size 4 \
+--gpu-memory-utilization 0.65
